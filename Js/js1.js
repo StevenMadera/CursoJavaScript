d132b7f4d15dd17ba89da392be66e74b2dd41b72
StevenMadera <maderasteven4@gmail.com>
2021-06-29 19:19:35 -0500

    Pagina #3

---
+++ Js/js1.js
@@ -0,0 +1,3 @@
+let nombre = prompt('Escribe tu Nombre')
+
+alert('Tu nombre es ' + nombre)
---
+++ Js/js2.js
@@ -0,0 +1,18 @@
+numero1 = 10;
+
+numero2 = 5;
+
+numero3 = 10;
+numero3--;
+
+numero4 = 10;
+numero4++;
+
+numero5 = 10;
+cuadrado = numero5**2;
+
+numero6 = 10;
+
+
+
+
